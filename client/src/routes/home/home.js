39c7777ab288b22415a05c27d22ec8860b7b4934
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-04 20:57:37 -0500

    home component to login

--- client/src/routes/home/home.js
+++ client/src/routes/home/home.js
@@ -3,7 +3,20 @@ import "./home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div id="home-container" className="d-flex justify-content-center align-items-center">
+        <div id="login-container">
+            <label className="center-label" id="auth-label">Authentication Page</label>
+            <hr/>
+            <label className="center-label">Connection String</label>
+            <input className="center-div" type="text"></input>
+            <label className="center-label">User ID/Name</label>
+            <input className="center-div" type="text"></input>
+            <label className="center-label">Password</label>
+            <input className="center-div"type="password"></input>
+            <hr/>
+            <div id="login-button">Login</div>
+        </div>
+    </div>
   );
 }
 
