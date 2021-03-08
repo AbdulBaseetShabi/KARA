d615b3b36283ebe6d89fb7f910d1eb41faa6c4fb
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-08 17:13:23 -0500

    centered nav items

--- client/src/widgets/navigation-bar/navigation-bar.js
+++ client/src/widgets/navigation-bar/navigation-bar.js
@@ -7,10 +7,10 @@ function NavigationBar(props) {
   }
   return (
 
-    <div class= "navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#1A3263" }}>
-        <a class="navbar" href="/table" style={{ color: "#f8f9fa"}} ><div>Table</div></a>
-        <a class="navbar" href="/databases" style={{ color: "#f8f9fa"}}><div>Database</div></a>
-        <a class="navbar" href="/Logout" style={{ color: "#f8f9fa"}}><div>Logout</div></a>
+    <div id="nav-container" class="row" style={{ backgroundColor: "#1A3263" }}>
+        <a class="col nav-link" href="/table" style={{ color: "#f8f9fa"}} ><div>Table</div></a>
+        <a class="col nav-link" href="/databases" style={{ color: "#f8f9fa"}}><div>Database</div></a>
+        <a class="col nav-link" href="/Logout" style={{ color: "#f8f9fa"}}><div>Logout</div></a>
     </div>
   )
 }
