32099a0bcecaf6ca74ffdc1934b2c02ac371bf12
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-09 11:24:28 -0500

    pop up to edit db name and create a new database

---
+++ client/src/routes/databases/db-edit-add-popup.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function AddDB(props) {
+  if (props.show) {
+    return (
+      <div className="modal-options">
+        <div className="modal-options-content">
+          <label className="center-label page-label">{props.is_add_new_db ? "Add New Database" : "Change Database Name"}</label>
+          <hr className="header-hr" />
+          <label className="metadata-label center-label">
+            Database Name{props.is_add_new_db ? "" : ": " + props.old_name}
+          </label>
+          <input
+            type="text"
+            style={{ marginBottom: "10px" }}
+            onChange={(e) => {
+              let value = e.target.value.trim();
+              if (props.is_add_new_db) {
+                props.controlCreateNewDatabase(value);
+              } else {
+                props.controlChangeDataBaseName(value);
+              }
+            }}
+          ></input>
+          <div className="row button-row">
+            <button
+              className="col"
+              type="button"
+              style={{ margin: "0 auto" }}
+              onClick={
+                props.loading
+                  ? null
+                  : () => {
+                      if (props.is_add_new_db) {
+                        props.createNewDatabase();
+                      } else {
+                        props.changeDataBaseName();
+                      }
+                    }
+              }
+            >
+              {props.loading ? (
+                <label style={{ paddingTop: "7px" }}>
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                    style={{ margin: "0 5px 3.5px 0px" }}
+                  ></span>
+                  Loading
+                </label>
+              ) : props.is_add_new_db ? (
+                "Create Database"
+              ) : (
+                "Change Name"
+              )}
+            </button>
+            <button
+              className="col"
+              type="button"
+              style={{ margin: "0 auto" }}
+              onClick={() => props.openModal(false, false, "")}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return null;
+}
+
+export default AddDB;
--- client/src/routes/databases/view/databases-view.js
+++ client/src/routes/databases/view/databases-view.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AddDB from "../db-edit-add-popup";
+import PopUp from "../../../widgets/pop-ups/pop-ups";
 import "./databases-view.css";
 
 const mock_data = [
@@ -15,12 +17,76 @@ class DatabasesView extends React.Component {
     super(props);
     this.state = {
       is_loading: false,
+      show_add_modal: false,
+      creating_new_db: false,
+      updating_db_name: false,
+      is_add_new_db: false,
+      response: {},
     };
+    this.new_db_name = "";
+    this.modified_db = {
+      previous_name: "",
+      current_name: "",
+    };
+    this.changeModalState = this.changeModalState.bind(this);
+    this.createNewDatabase = this.createNewDatabase.bind(this);
+    this.changeDataBaseName = this.changeDataBaseName.bind(this);
+    this.updatePopUp = this.updatePopUp.bind(this);
+    this.controlCreateNewDataBase = this.controlCreateNewDataBase.bind(this);
+    this.controlChangeDataBaseName = this.controlChangeDataBaseName.bind(this);
   }
 
+  changeModalState(state, add_new_or_edit_old, old_name) {
+    if (!add_new_or_edit_old) {
+      this.modified_db.previous_name = old_name;
+    }
+    this.setState({
+      show_add_modal: state,
+      is_add_new_db: add_new_or_edit_old,
+    });
+  }
+
+  controlCreateNewDataBase(value) {
+    this.new_db_name = value;
+  }
+
+  controlChangeDataBaseName(value) {
+    this.modified_db.current_name = value;
+  }
+
+  changeDataBaseName() {
+    console.log(this.modified_db);
+    if (this.modified_db.current_name === this.modified_db.previous_name) {
+      this.updatePopUp({ type: "error", message: "Using this same name" });
+    } else if (this.modified_db.current_name.length === 0) {
+      this.updatePopUp({ type: "success", message: "No name specified" });
+    } else {
+      this.setState({ updating_db_name: true });
+    }
+  }
+
+  createNewDatabase() {
+    console.log(this.new_db_name);
+    if (this.new_db_name.length === 0) {
+      this.updatePopUp({ type: "warning", message: "No name specified" });
+    } else {
+      this.setState({ creating_new_db: true });
+    }
+  }
+
+  updatePopUp(response) {
+    this.setState({ response: response });
+  }
   render() {
     return (
       <div id="db-view-container">
+        <PopUp
+          modaltype={this.state.response.type}
+          show={Object.keys(this.state.response).length !== 0}
+          closePopUp={this.updatePopUp}
+        >
+          {this.state.response.message}
+        </PopUp>
         <label className="center-label page-label">
           Databases in Current Server
         </label>
@@ -29,7 +95,9 @@ class DatabasesView extends React.Component {
           {mock_data.map((db, index) => {
             return (
               <div key={index} className="col-4 custom-card">
-                <label className="center-label db-name-label">{db.db_name}</label>
+                <label className="center-label db-name-label">
+                  {db.db_name}
+                </label>
                 <hr className="header-hr" />
                 <label>Tables: {db.no_of_tables} found in the database</label>
                 <br />
@@ -40,6 +108,12 @@ class DatabasesView extends React.Component {
                 <a href="/table">
                   <div className="button view-more-button">View more info</div>
                 </a>
+                <div
+                  className="button delete-button"
+                  onClick={() => this.changeModalState(true, false, db.db_name)}
+                >
+                  Change Name
+                </div>
                 <a href="/databases">
                   <div className="button delete-button">DELETE</div>
                 </a>
@@ -47,13 +121,29 @@ class DatabasesView extends React.Component {
             );
           })}
           <div key={mock_data.length} className="col-4 custom-card">
-            <a href="/databases">
-              <label className="center-label" id="add-new-db-label">ADD A NEW DATABASE</label>
-              <hr className="header-hr" />
-              <i className="fas fa-plus fa-7x big-plus"></i>
-            </a>
+            <label className="center-label" id="add-new-db-label">
+              ADD A NEW DATABASE
+            </label>
+            <hr className="header-hr" />
+            <i
+              className="fas fa-plus fa-7x big-plus"
+              onClick={() => {
+                this.changeModalState(true, true, "");
+              }}
+            ></i>
           </div>
         </div>
+        <AddDB
+          show={this.state.show_add_modal}
+          openModal={this.changeModalState}
+          loading={this.state.creating_new_db || this.state.updating_db_name}
+          controlCreateNewDatabase={this.controlCreateNewDataBase}
+          controlChangeDataBaseName={this.controlChangeDataBaseName}
+          createNewDatabase={this.createNewDatabase}
+          changeDataBaseName={this.changeDataBaseName}
+          is_add_new_db={this.state.is_add_new_db}
+          old_name={this.modified_db.previous_name}
+        />
       </div>
     );
   }
