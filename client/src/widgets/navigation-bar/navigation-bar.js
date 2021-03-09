00efc7bca076fc25e616c75462a859a57f55b0d7
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-08 21:07:46 -0500

    centered nav bar

--- client/src/widgets/navigation-bar/navigation-bar.js
+++ client/src/widgets/navigation-bar/navigation-bar.js
@@ -7,10 +7,10 @@ function NavigationBar(props) {
   }
   return (
 
-    <div id="nav-container" class="row" style={{ backgroundColor: "#1A3263" }}>
-        <a class="col nav-link" href="/table" style={{ color: "#f8f9fa"}} ><div>Table</div></a>
-        <a class="col nav-link" href="/databases" style={{ color: "#f8f9fa"}}><div>Database</div></a>
-        <a class="col nav-link" href="/Logout" style={{ color: "#f8f9fa"}}><div>Logout</div></a>
+    <div id="nav-container" className="row" style={{ backgroundColor: "#1A3263" }}>
+        <a className="col nav-link" href="/table" style={{ color: "#f8f9fa"}} ><div>Table</div></a>
+        <a className="col nav-link" href="/databases" style={{ color: "#f8f9fa"}}><div>Database</div></a>
+        <a className="col nav-link" href="/Logout" style={{ color: "#f8f9fa"}}><div>Logout</div></a>
     </div>
   )
 }
