0933b113af79c4a15cf1abe099dd5ff8e6b5b743
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-09 01:49:16 -0500

    implementing back button and code refactor

--- client/src/widgets/navigation-bar/navigation-bar.js
+++ client/src/widgets/navigation-bar/navigation-bar.js
@@ -1,18 +1,41 @@
 import React from "react";
 import "./navigation-bar.css";
 
+function previousPath(currentPath) {
+  window.history.back();
+}
+
 function NavigationBar(props) {
-  if (props.currentPath === '/home') {
+  if (props.currentPath === "/home") {
     return null;
   }
   return (
-
-    <div id="nav-container" className="row" style={{ backgroundColor: "#1A3263" }}>
-        <a className="col nav-link" href="/table" style={{ color: "#f8f9fa"}} ><div>Table</div></a>
-        <a className="col nav-link" href="/databases" style={{ color: "#f8f9fa"}}><div>Database</div></a>
-        <a className="col nav-link" href="/Logout" style={{ color: "#f8f9fa"}}><div>Logout</div></a>
+    <div
+      id="nav-container"
+      className="row"
+      style={{ backgroundColor: "#1A3263" }}
+    >
+      {props.currentPath !== "/databases/view" && window.history.length > 1 ? (
+        <div
+          className="col-1"
+          onClick={() => {
+            previousPath(props.currentPath);
+          }}
+        >
+          <i id="back-arrow" className="fas fa-arrow-left fa-2x"></i>
+        </div>
+      ) : null}
+      <a className="col nav-link" href="/table">
+        <div>Table</div>
+      </a>
+      <a className="col nav-link" href="/databases">
+        <div>Database</div>
+      </a>
+      <a className="col nav-link" href="/Logout">
+        <div>Logout</div>
+      </a>
     </div>
-  )
+  );
 }
 
 export default NavigationBar;
