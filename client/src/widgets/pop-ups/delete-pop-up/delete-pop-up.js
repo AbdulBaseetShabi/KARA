83d0c54de4d23831719c1144b9c6da86efa92dfb
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-09 12:47:46 -0500

    prompt for deletion

---
+++ client/src/widgets/pop-ups/delete-pop-up/delete-pop-up.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function DeletePopUp(props) {
+  if (props.show) {
+    return (
+      <div id="delete-prompt-content" className="modal-options">
+        <div className="modal-options-content">
+          <label className="center-label page-label">Delete Confirmation</label>
+          <hr className="header-hr" />
+          <label className="center-label">
+            Are you sure you want to delete this {props.delete_name}
+          </label>
+          <div className="row button-row">
+            <button
+              className="col"
+              type="button"
+              style={{ margin: "0 auto" }}
+              onClick={
+                props.loading
+                  ? null
+                  : () => {
+                      props.deleteData(props.data);
+                    }
+              }
+            >
+              {props.loading ? (
+                <label style={{ paddingTop: "7px" }}>
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                    style={{ margin: "0 5px 3.5px 0px" }}
+                  ></span>
+                  Loading
+                </label>
+              ) : (
+                "Delete " + props.delete_name
+              )}
+            </button>
+            <button
+              className="col"
+              type="button"
+              style={{ margin: "0 auto" }}
+              onClick={() => props.openModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default DeletePopUp;
