ef984a3d9a4efadca4fd395b46e7a29bfe08dafa
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-08 22:04:51 -0500

    pop up message for server side responses

---
+++ client/src/widgets/pop-ups/pop-ups.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./pop-ups.css";
+
+function PopUp(props) {
+  if (props.show) {
+    return (
+      <div
+        id="pop-up-content"
+        style={{
+          backgroundColor:
+            props.modaltype === "error"
+              ? "#cc3300"
+              : props.modaltype === "warning"
+              ? "#ffcc00"
+              : "#99cc33",
+        }}
+      >
+        <div id="popup-message">{props.children}</div>
+        <label  id="close-icon" onClick={()=>{props.closePopUp({});}}>Close  <i class="fas fa-times"></i></label>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default PopUp;
