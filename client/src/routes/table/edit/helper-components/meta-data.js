e13e24cc895a047ab3f9103917dee8dcd138f509
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-09 01:20:08 -0500

    moved file

--- client/src/routes/table/edit/helper-components/meta-data.js
+++ client/src/routes/table/edit/helper-components/meta-data.js
@@ -3,10 +3,9 @@ import React from "react";
 class MetaData extends React.Component {
   render() {
     return (
-      <div class="white-card">
+      <div className="white-card">
         <label className="center-label page-label">
           Meta Data
-          {/* <i class="fas fa-plus"></i> */}
           <label
             className="close-open-button"
             onClick={() => {
@@ -19,24 +18,24 @@ class MetaData extends React.Component {
         {this.props.open_meta_data_view ? (
           <div>
             <hr className="header-hr" />
-            <div class="metadata">
-              <label class="metadata-label">Database Name</label>
+            <div className="metadata">
+              <label className="metadata-label">Database Name</label>
               <input type="text"></input>
             </div>
-            <div class="metadata">
-              <label class="metadata-label">Table Name</label>
+            <div className="metadata">
+              <label className="metadata-label">Table Name</label>
               <input type="text"></input>
             </div>
-            <div class="metadata">
-              <label class="metadata-label">Number of Entries</label>
+            <div className="metadata">
+              <label className="metadata-label">Number of Entries</label>
               <input
                 type="text"
                 disabled="disabled"
                 value={this.props.table_meta_data.no_of_entries}
               ></input>
             </div>
-            <div class="metadata">
-              <label class="metadata-label">Number of Columns</label>
+            <div className="metadata">
+              <label className="metadata-label">Number of Columns</label>
               <input
                 type="text"
                 disabled="disabled"
