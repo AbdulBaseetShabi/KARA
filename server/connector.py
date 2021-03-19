ef518436932628bd3cef311f8c2a2c81e7a70fd3
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-19 00:15:24 -0400

    autocommit transaction

--- server/connector.py
+++ server/connector.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 class Connector:
-    def __init__(self, driver="MySQL ODBC 8.0 Unicode Driver", server="localhost", uid="root", password=""):
+    def __init__(self, driver="MySQL ODBC 8.0 Unicode Driver", server="localhost", uid="root", password="", autocommit=False):
         connection_string = (
             f'DRIVER={driver};'
             f'SERVER={server};'
@@ -10,8 +10,8 @@ class Connector:
             'charset=utf8mb4;'
             'Trusted_Connection=yes;'
         )
-        print(connection_string)
-        self._conn = pyodbc.connect(connection_string)
+
+        self._conn = pyodbc.connect(connection_string, autocommit=autocommit)
         self._cursor = self._conn.cursor()
 
     def execute(self, query, values=None):
@@ -40,12 +40,11 @@ class Connector:
         return self._cursor.description
 
     def check_db(self, name):
-        self._cursor.execute("SHOW DATABASES")
+        self._cursor.execute("SELECT name FROM sys.databases")
 
         for (db,) in self._cursor:
-            if db == name:
+            if db.lower() == name.lower():
                 return True
-    
         return False
 
     def check_table(self, db, name):
